0fbaa1ade7125be01ac23e8ed66931714c1b5844
Barbara Z Johnson <umdbzj@gmail.com>
2019-08-09 11:34:37 -0500

    Add page titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,36 +4,43 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "CSC 105 Home"
   end
 
   test "should get schedule" do
     get static_pages_schedule_url
     assert_response :success
+    assert_select "title", "CSC 105 Schedule"
   end
 
   test "should get labs" do
     get static_pages_labs_url
     assert_response :success
+    assert_select "title", "CSC 105 Labs"
   end
 
   test "should get homework" do
     get static_pages_homework_url
     assert_response :success
+    assert_select "title", "CSC 105 Homework"
   end
 
   test "should get deadlines" do
     get static_pages_deadlines_url
     assert_response :success
+    assert_select "title", "CSC 105 Deadlines"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "CSC 105 Help"
   end
   
   test "should get resources" do
   	get static_pages_resources_url
   	assert_response :success
+  	assert_select "title", "CSC 105 Resources"
   end
 
 end
