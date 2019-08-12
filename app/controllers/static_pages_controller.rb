4124b422b6fc7c9d9ace0931c3030f86a2b97e63
Barbara Z Johnson <umdbzj@gmail.com>
2019-08-12 14:46:15 -0500

    Add Ruby interactivity to contact page.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -21,6 +21,29 @@ class StaticPagesController < ApplicationController
   end
   
   def contact
+  	nameList = ["Professor Johnson", "Barbara", "Dr. Johnson", "Ms. Johnson"]
+  	nameNum = (0..nameList.length-1).to_a.shuffle.first
+	@name = nameList[nameNum]
+	
+	adjList = ["usually", "reliably", "most often", "frequently", "indubitably"]
+	adjNum = (0..adjList.length-1).to_a.shuffle.first
+	@adj = adjList[adjNum]
+	
+	profList = ["teaching", "attending meetings", "serving on a committee", "in office hours"]
+	profNum = (0..profList.length-1).to_a.shuffle.first
+	@profaction = profList[profNum]
+	
+	advList = ["happily", "actively", "carefully", "intently"]
+	advNum = (0..advList.length-1).to_a.shuffle.first
+	@adv = advList[advNum]
+	
+	actList = ["practicing music", "catching Pokemon", "turning coffee into code", "claiming portals for the Resistance", "returning Foundables"]
+	actNum = (0..actList.length-1).to_a.shuffle.first
+	@act = actList[actNum]
+  	
+  	@result = (1..99).to_a.shuffle.first
+  	@result2 = (1..99).to_a.shuffle.first
+  	
   end
   
   def about
